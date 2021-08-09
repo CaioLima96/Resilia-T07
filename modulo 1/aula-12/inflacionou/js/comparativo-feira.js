10e21dd17b2aca59982afb8e838f41c4be1ae82d
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-08-09 14:06:33 -0300

    concertado erro de nome no <p> e mudando a height do #produtos aula 12 modulo 1

--- modulo 1/aula-12/inflacionou/js/comparativo-feira.js
+++ modulo 1/aula-12/inflacionou/js/comparativo-feira.js
@@ -30,16 +30,19 @@ function laranja () {
 
         var valorDiferenca = valorAtual - valorAntigo;
 
-        document.getElementById("laranja").innerHTML = `O tomate teve um aumento de:R$ ${valorDiferenca}`;
+        document.getElementById("laranja").innerHTML = `A laranja teve um aumento de: R$ ${valorDiferenca}`;
+        document.getElementById("laranja").style.marginTop = "10px";
 
     } else if (+valorAntigo > +valorAtual ) {
 
         var valorDiferenca = valorAntigo - valorAtual;
 
-        document.getElementById("laranja").innerHTML = `O tomate teve uma redução de:R$ ${valorDiferenca}`;
+        document.getElementById("laranja").innerHTML = `A laranja teve uma redução de: R$ ${valorDiferenca}`;
+        document.getElementById("laranja").style.marginTop = "10px";
         
     } else {
-        document.getElementById("laranja").innerHTML = "O tomate não teve alteração de preço.";
+        document.getElementById("laranja").innerHTML = "A laranja não teve alteração de preço.";
+        document.getElementById("laranja").style.marginTop = "10px";
     }
 }
 
@@ -51,16 +54,19 @@ function abacaxi () {
 
         var valorDiferenca = valorAtual - valorAntigo;
 
-        document.getElementById("abacaxi").innerHTML = `O tomate teve um aumento de:R$ ${valorDiferenca}`;
+        document.getElementById("abacaxi").innerHTML = `O abacaxi teve um aumento de: R$ ${valorDiferenca}`;
+        document.getElementById("abacaxiabacaxi").style.marginTop= "10px";
 
     } else if (+valorAntigo > +valorAtual ) {
 
         var valorDiferenca = valorAntigo - valorAtual;
 
-        document.getElementById("abacaxi").innerHTML = `O tomate teve uma redução de:R$ ${valorDiferenca}`;
+        document.getElementById("abacaxi").innerHTML = `O abacaxi teve uma redução de: R$ ${valorDiferenca}`;
+        document.getElementById("abacaxi").style.marginTop = "10px";
         
     } else {
-        document.getElementById("abacaxi").innerHTML = "O tomate não teve alteração de preço.";
+        document.getElementById("abacaxi").innerHTML = "O abacaxi não teve alteração de preço.";
+        document.getElementById("abacaxi").style.marginTop = "10px";
     }
 }
 
@@ -72,15 +78,18 @@ function limao () {
 
         var valorDiferenca = valorAtual - valorAntigo;
 
-        document.getElementById("limao").innerHTML = `O tomate teve um aumento de:R$ ${valorDiferenca}`;
+        document.getElementById("limao").innerHTML = `O limão teve um aumento de: R$ ${valorDiferenca}`;
+        document.getElementById("limao").style.marginTop = "10px";
 
     } else if (+valorAntigo > +valorAtual ) {
 
         var valorDiferenca = valorAntigo - valorAtual;
 
-        document.getElementById("limao").innerHTML = `O tomate teve uma redução de:R$ ${valorDiferenca}`;
+        document.getElementById("limao").innerHTML = `O limão teve uma redução de: R$ ${valorDiferenca}`;
+        document.getElementById("limao").style.marginTop = "10px";
         
     } else {
-        document.getElementById("limao").innerHTML = "O tomate não teve alteração de preço.";
+        document.getElementById("limao").innerHTML = "O limão não teve alteração de preço.";
+        document.getElementById("limao").style.marginTop = "10px";
     }
 }
