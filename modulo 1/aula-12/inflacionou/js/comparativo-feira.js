27ea608df3f9d7b5a7722a92b5f58c22377551c8
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-08-09 11:52:40 -0300

    adicionado bg para o inflacionou da aula 12 modulo 1

--- modulo 1/aula-12/inflacionou/js/comparativo-feira.js
+++ modulo 1/aula-12/inflacionou/js/comparativo-feira.js
@@ -6,15 +6,15 @@ function tomate () {
 
         var valorDiferenca = valorAtual - valorAntigo;
 
-        document.getElementById("tomate").innerHTML = `O tomate teve um aumento de: ${valorDiferenca}`;
+        document.getElementById("tomate").innerHTML = `O tomate teve um aumento de: R$ ${valorDiferenca}`;
 
     } else if (+valorAntigo > +valorAtual ) {
 
         var valorDiferenca = valorAntigo - valorAtual;
 
-        document.getElementById("tomate").innerHTML = `O tomate teve uma redução de: ${valorDiferenca}`;
+        document.getElementById("tomate").innerHTML = `O tomate teve uma redução de: R$ ${valorDiferenca}`;
         
-    } else {
+    } else{
         document.getElementById("tomate").innerHTML = "O tomate não teve alteração de preço.";
     }
 }
@@ -27,13 +27,13 @@ function laranja () {
 
         var valorDiferenca = valorAtual - valorAntigo;
 
-        document.getElementById("laranja").innerHTML = `O tomate teve um aumento de: ${valorDiferenca}`;
+        document.getElementById("laranja").innerHTML = `O tomate teve um aumento de:R$ ${valorDiferenca}`;
 
     } else if (+valorAntigo > +valorAtual ) {
 
         var valorDiferenca = valorAntigo - valorAtual;
 
-        document.getElementById("laranja").innerHTML = `O tomate teve uma redução de: ${valorDiferenca}`;
+        document.getElementById("laranja").innerHTML = `O tomate teve uma redução de:R$ ${valorDiferenca}`;
         
     } else {
         document.getElementById("laranja").innerHTML = "O tomate não teve alteração de preço.";
@@ -48,13 +48,13 @@ function abacaxi () {
 
         var valorDiferenca = valorAtual - valorAntigo;
 
-        document.getElementById("abacaxi").innerHTML = `O tomate teve um aumento de: ${valorDiferenca}`;
+        document.getElementById("abacaxi").innerHTML = `O tomate teve um aumento de:R$ ${valorDiferenca}`;
 
     } else if (+valorAntigo > +valorAtual ) {
 
         var valorDiferenca = valorAntigo - valorAtual;
 
-        document.getElementById("abacaxi").innerHTML = `O tomate teve uma redução de: ${valorDiferenca}`;
+        document.getElementById("abacaxi").innerHTML = `O tomate teve uma redução de:R$ ${valorDiferenca}`;
         
     } else {
         document.getElementById("abacaxi").innerHTML = "O tomate não teve alteração de preço.";
@@ -69,38 +69,15 @@ function limao () {
 
         var valorDiferenca = valorAtual - valorAntigo;
 
-        document.getElementById("limao").innerHTML = `O tomate teve um aumento de: ${valorDiferenca}`;
+        document.getElementById("limao").innerHTML = `O tomate teve um aumento de:R$ ${valorDiferenca}`;
 
     } else if (+valorAntigo > +valorAtual ) {
 
         var valorDiferenca = valorAntigo - valorAtual;
 
-        document.getElementById("limao").innerHTML = `O tomate teve uma redução de: ${valorDiferenca}`;
+        document.getElementById("limao").innerHTML = `O tomate teve uma redução de:R$ ${valorDiferenca}`;
         
     } else {
         document.getElementById("limao").innerHTML = "O tomate não teve alteração de preço.";
     }
 }
-
-// function () {
-//     var alimento = prompt("digita ae")
-// var valorAntigo = prompt("Digite o valor antigo: ")
-//     var valorAtual = prompt("Digite o valor atual: ")
-
-//     if (alimento == "tomate" && valorAtual > valorAntigo) {
-
-//         var valorDiferenca = valorAtual - valorAntigo;
-
-//         alert( `O tomate teve um aumento de: ${valorDiferenca}`)
-
-//     } else if (alimento == "tomate" && valorAntigo > valorAtual ) {
-
-//         var valorDiferenca = valorAntigo - valorAtual;
-
-//         alert( `O tomate teve um aumento de: ${valorDiferenca}`)
-        
-//     } else {
-//         alert("Teve mudança não po")
-//     }
-// }
-
