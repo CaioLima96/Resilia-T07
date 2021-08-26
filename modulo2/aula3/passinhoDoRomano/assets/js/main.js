4da7900e2b7800a9d19e2a7b49345298b54359f7
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-08-26 13:21:00 -0300

    adicionado o toogle pro incremento aula 03 mod2

--- modulo2/aula3/passinhoDoRomano/assets/js/main.js
+++ modulo2/aula3/passinhoDoRomano/assets/js/main.js
@@ -1,6 +1,17 @@
-function cesarCifra() {
-    var cifraSelec = document.querySelector('form')
-    var createRadioCesar = document.createElement('input')
-    cifraSelec.appendChild(createRadioCesar)
-    
-}
+var incrementar = document.querySelector('.incremento')
+document.querySelector('#caio').addEventListener('click', function() {
+    incrementar.classList.toggle('incrementoNone')
+})
+
+
+// var rad = document.getElementsByClassName('CrifraSelec').document.getElementsByName('cesar').value('')
+
+// function hideIncremento() {
+
+//     // if (incrementar.style.display === "none") {
+//     //     incrementar.style.display = "flex"
+//     // } else {
+//     //     incrementar.style.display = "none"
+//     // }
+
+// }
