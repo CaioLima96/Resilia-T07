be1acea0ee474aa336733022b1c065723fc56777
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-08-29 22:32:08 -0300

    cifra de cesar feitoooooooo

--- modulo2/aula3/passinhoDoRomano/assets/js/main.js
+++ modulo2/aula3/passinhoDoRomano/assets/js/main.js
@@ -54,59 +54,146 @@ function showDecodeBtn() {
 ==========================================
 */
 
+//PEGAR AUTOMATICO DO TEXTAREA 1 PARA O TEXTAREA 2
 
-let areaTextInicio = document.getElementById('textoInicial')
-let areaTextFim = document.getElementById('textoFinal')
+// let areaTextInicio = document.getElementById('textoInicial')
+// let areaTextFim = document.getElementById('textoFinal')
 
-areaTextInicio.addEventListener('keydown', () => {
-    let texto = document.getElementById('textoInicial').value
-    areaTextFim.innerText = texto
-    console.log('abc')
-})
+// areaTextInicio.addEventListener('keydown', () => {
+//     let texto = document.getElementById('textoInicial').value
+//     areaTextFim.innerText = texto
+//     console.log('abc')
+// })
 
 
 /*
 ==========================================
 */
 
+
+//CAESAR CIPHER
 
 function caesarCipher(str, num) {
 
     num = num % 26
+
     var lowerCaseStr = str.toLowerCase();
     var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
     var newStr = '';
 
-    for(var i =0; i < lowerCaseStr.length; i++) {
+    console.log(str)
+
+    for(var i = 0; i < lowerCaseStr.length; i++) {
         var currentLetter = lowerCaseStr[i]
         if (currentLetter === ' ') {
             newStr += currentLetter;
+            console.log('jjjj')
             continue;
         }
+
         var currentIndex = alphabet.indexOf(currentLetter)
         var newIndex = currentIndex + num
-        if (newIndex > 25) newIndex = newIndex - 26
-        if (newIndex < 0) newIndex = newIndex + 26
+
+        if (newIndex > 25) {
+            newIndex = newIndex - 26 
+            console.log('pppp')
+        } else if (newIndex < 0) {
+            newIndex = newIndex + 26
+            console.log('vvvvv')
+        }
+        
         if(str[i] === str[i].toUpperCase()) {
             newStr += alphabet[newIndex].toUpperCase()
+            console.log('nnnn')
         } else {
             newStr += alphabet[newIndex]
+            console.log('kkkk')
         }
 
     }
-
+    
+    console.log(newStr)
     return newStr
 }
 
+document.getElementById('btnCode').addEventListener('click', function() {
+    var inserirTexto = document.getElementById('textoFinal')
+    console.log('coca-cola')
 
-console.log(caesarCipher('Caio Vieira Lima', 3))
+    var str = document.getElementById('textoInicial').value
+    var num = document.getElementById('incremento').value
+
+    
+    inserirTexto.innerText = caesarCipher(str, num)
+})
+
+document.getElementById('btnDecode').addEventListener('click', function() {
+    var inserirTexto = document.getElementById('textoFinal')
+    console.log('flexa')
+    
+    var str = document.getElementById('textoInicial').value
+    var num = document.getElementById('incremento').value
+    num *= -1
+
+    
+    inserirTexto.innerText = caesarCipher(str, num)
+})
 
 
+
+//BASE 64
 
 
 /*
 ==========================================
 */
 
+
+// function caesarCipher() {
+
+//     var str = document.getElementById('textoInicial').value
+//     var num = document.getElementById('incremento').value
+//     num = num % 26
+
+//     var lowerCaseStr = str.toLowerCase();
+//     var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+//     var newStr = '';
+
+//     console.log(str)
+
+//     for(var i = 0; i < lowerCaseStr.length; i++) {
+//         var currentLetter = lowerCaseStr[i]
+//         if (currentLetter === ' ') {
+//             newStr += currentLetter;
+//             console.log('jjjj')
+//             continue;
+//         }
+
+//         var currentIndex = alphabet.indexOf(currentLetter)
+//         var newIndex = currentIndex + num
+
+//         if (newIndex > 25) {
+//             newIndex = newIndex - 26 
+//             console.log('pppp')
+//         } else if (newIndex < 0) {
+//             newIndex = newIndex + 26
+//             console.log('vvvvv')
+//         }
+        
+//         if(str[i] === str[i].toUpperCase()) {
+//             newStr += alphabet[newIndex].toUpperCase()
+//             console.log('nnnn')
+//         } else {
+//             newStr += alphabet[newIndex]
+//             console.log('kkkk')
+//         }
+
+//     }
+    
+//     console.log(newStr)
+//     return newStr
+// }
+
+
+
 // let chave = +document.getElementById('#incremento').value
-// let areaTextSplit = areaText.split('')
