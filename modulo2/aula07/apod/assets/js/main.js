e613fd27af4f6833c7d3c33d89dd47248d5e9e92
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-09-04 21:32:39 -0300

    js do apod  feito aula 07 mod02

--- modulo2/aula07/apod/assets/js/main.js
+++ modulo2/aula07/apod/assets/js/main.js
@@ -1,7 +1,45 @@
-const nasa = $.ajax({url: 'https://api.nasa.gov/planetary/apod?api_key=WgSfzp8CJwi22ZaHEHUyD4hBuVuiAfeKb6tHk7dB', async: false}).responseJSON
+const btnSubimit = document.querySelector('#btnSubmit').addEventListener('click', (event) => {
+    event.preventDefault()
 
-espaco.innerHTML = `
-	<h1> ${nasa.title} </h1>
-`
+    let dataValue = document.getElementById('data')
 
-console.log(nasa)
+    let nasaApi = $.ajax({url: `https://api.nasa.gov/planetary/apod?api_key=WgSfzp8CJwi22ZaHEHUyD4hBuVuiAfeKb6tHk7dB&date=${dataValue.value}`, async: false}).responseJSON
+    
+    if(nasaApi.code == 400) {
+
+        mainContent.innerHTML = `
+        <h1>Selecine uma data igual ou anterior à data atual. Vemos o espaço, mas não o futuro para tirar foto do amanhã. :)</h1>
+        `
+
+    } else {
+
+        mainContent.innerHTML = `
+        
+        
+
+        <div id="nasa-content">
+            <div>
+                <img id="nasa-img" src="${nasaApi.url}" alt="${nasaApi.title}">
+            </div>
+            
+            <div>
+                <h3> ${nasaApi.title} </h3>
+                <p style="margin: 1rem 0;">${nasaApi.explanation}</p>
+                <p>Image credits & copyright: ${nasaApi.copyright ? nasaApi.copyright : 'Autor desconhecido'}</p>
+            </div>
+        </div>
+        
+        `
+    
+
+        console.log(nasaApi)
+    }
+
+    
+})
+
+
+
+
+
+
